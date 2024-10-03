17a17b081707d35698b2485c8a224a06a4f071eb
alpylmz <ylmz.alp.e@gmail.com>
2024-10-03 13:33:47 -0400

    input unrolling works

--- src/main/scala/daisy/transform/ArithmeticUnrollPhase.scala
+++ src/main/scala/daisy/transform/ArithmeticUnrollPhase.scala
@@ -8,7 +8,7 @@ import daisy.lang.Identifiers._
 import daisy.lang.TreeOps
 import daisy.lang.TreeOps.{getLastExpression, isMatrix, isVector, replace}
 import daisy.lang.Trees._
-import daisy.lang.Types.{FinitePrecisionType, MatrixType, RealType, VectorType}
+import daisy.lang.Types.{FinitePrecisionType, MatrixType, RealType, VectorType, TypeTree}
 import daisy.tools.FinitePrecision.Precision
 import daisy.tools.{DSAbstraction, Interval, Rational}
 
@@ -29,7 +29,25 @@ object ArithmeticUnrollPhase extends DaisyPhase {
   override val description = "Unrolls all loops over data structures."
   override implicit val debugSection = DebugSectionTransform
 
+  var freshIdentifierList: List[Identifier] = List()
 
+  def getOrCreateFreshIdentifier(name: String, tpe: TypeTree): Identifier = {
+    // if the name is already in the list, return it
+    // otherwise, create a new one and return it
+    val id = freshIdentifierList.find(_.name == name)
+    id match {
+      case Some(x) => {
+        val () = println("Found in the list: " + x)
+        x
+      }
+      case None => {
+        val newId = FreshIdentifier(name, tpe)
+        val () = println("Not found in the list, creating a new one: " + newId)
+        freshIdentifierList = freshIdentifierList :+ newId
+        newId
+      }
+    }
+  }
 
   def getInputDSSize(ctx: Context, fnc: FunDef, x: ValDef): Int = {
     // This function gets the size of x in the spec
@@ -66,7 +84,10 @@ object ArithmeticUnrollPhase extends DaisyPhase {
     var specInputRanges: Map[Identifier, Map[Identifier, Interval]] = ctx.specInputRanges
     var specInputErrors: Map[Identifier, Map[Identifier, Rational]] = ctx.specInputErrors
 
+    var newspecInputRanges: Map[Identifier, Map[Identifier, Interval]] = Map()
+    val () = println("specInputRanges: " + specInputRanges)
     val newDefs = fncsToConsider.map(fnc => {
+      var specInputRangeForFunc: Map[Identifier, Interval] = Map()
       val () = println("fnc: " + fnc)
       val () = println("fnc.params: " + fnc.params)
       // fnc params is just a list
@@ -89,23 +110,23 @@ object ArithmeticUnrollPhase extends DaisyPhase {
           //val () = println("ctx.dsAbstractions(fnc.id)(vd.id): " + ctx.dsAbstractions(fnc.id)(vd))
           val () = println("specInputRanges: " + specInputRanges)
           val () = println("specInputRanges(fnc.id): " + specInputRanges(fnc.id))
-            val zero = 0
-            var currList = List(FreshIdentifier(s"${vd.id}$zero", RealType))
-            for(j <- 1 to vdSize){
-                val newId = FreshIdentifier(s"${vd.id}$j", RealType)
-                val newLet = FreshIdentifier(s"${vd.id}_$j")
-                currList = currList :+ newId
-                unrolledParamsFlattened = unrolledParamsFlattened :+ ValDef(newId)
-            }
-            currList
+          //var currList = List(FreshIdentifier(s"${vd.id}_0", RealType))
+          var currList = List(getOrCreateFreshIdentifier(s"${vd.id}_0", RealType))
+          unrolledParamsFlattened = unrolledParamsFlattened :+ ValDef(currList(0))
+          for(j <- 1 to vdSize - 1){
+              val newId = getOrCreateFreshIdentifier(s"${vd.id}_$j", RealType)
+              val newLet = getOrCreateFreshIdentifier(s"${vd.id}_$j", RealType)
+              currList = currList :+ newId
+              unrolledParamsFlattened = unrolledParamsFlattened :+ ValDef(newId)
+          }
+          currList
         case MatrixType(_) =>
           val mSize = specInputRanges(fnc.id)(vd.id)
-            val zero = 0
-            var currList = List(FreshIdentifier(s"${vd.id}$zero", RealType))
+            var currList = List(getOrCreateFreshIdentifier(s"${vd.id}_0", RealType))
             val () = println("ERROR: Matrix unrolling not supported yet")
             for(j <- 1 to 5){
-                val newId = FreshIdentifier(s"${vd.id}$j", RealType)
-                val newLet = FreshIdentifier(s"${vd.id}_$j")
+                val newId = getOrCreateFreshIdentifier(s"${vd.id}_$j", RealType)
+                val newLet = getOrCreateFreshIdentifier(s"${vd.id}_$j", RealType)
                 currList = currList :+ newId
                 unrolledParamsFlattened = unrolledParamsFlattened :+ ValDef(newId)
             }
@@ -117,9 +138,48 @@ object ArithmeticUnrollPhase extends DaisyPhase {
       
       val () = println("fnc.precondition: " + fnc.precondition)
       // we also need to overwrite the preconditions, again by unrolling
-      val newPrecondition = 
+      // A typical precondition:
+      // fnc.precondition: Some(((x ? -62.54) ? (x ? 15.02) ? x.(4) ? Tree? (class daisy.lang.Trees$VectorRange) ? Tree? (class daisy.lang.Trees$VectorRange) ? Tree? (class daisy.lang.Trees$VectorRange) ? Tree? (class daisy.lang.Trees$VectorRange)))
+      // I'll only use VectorRange, others seems useless
+      var newPreconditionFlattened: List[Expr] = List()
+      val newPrecondition = fnc.precondition.map(precond => precond match{
+        case And(exprs) => {
+          val () = println("exprs: " + exprs)
+          exprs.map(expr => expr match{
+            case VectorRange(v, fromInd, toInd, lb, ub) =>
+              val () = println("expr: " + expr)
+              if(fromInd != toInd){
+                val () = println("ERROR: fromInd and toInd should be the same, the other case is not supported")
+              }
+              val vId = v match{
+                case VectorLiteral(id) => id
+                case _ => {
+                    val () = println("ERROR: Not a VectorLiteral")
+                    ""
+                }
+              }
+              val unrollId = getOrCreateFreshIdentifier(s"${vId}_$fromInd", RealType)
+              val () = println("unrollId: " + unrollId)
+              newPreconditionFlattened = newPreconditionFlattened :+ GreaterThan(Variable(unrollId), lb)
+              newPreconditionFlattened = newPreconditionFlattened :+ LessThan(Variable(unrollId), ub)
+              specInputRangeForFunc = specInputRangeForFunc + (unrollId -> Interval(lb.value, ub.value))
+              val () = println("specInputRangeForFunc: " + specInputRangeForFunc)
+            case _ => {
+                val () = println("ERROR: Not a VectorRange")
+                val () = println("the type is: " + precond.getClass)
+                val () = println("expr: " + expr)
+            }
+          })  
+        }
 
+      })
+      //val () = println("newPrecondition: " + newPreconditionFlattened)
+      fnc.precondition = Some(And(newPreconditionFlattened))
+      val () = println("fnc.precondition: " + fnc.precondition)
+      val () = println("newspecInputRanges: " + newspecInputRanges)
+              val () = println("specInputRangeForFunc: " + specInputRangeForFunc)
 
+      newspecInputRanges = newspecInputRanges + (fnc.id -> specInputRangeForFunc)
 
       val transformed = unrollAll(fnc.body.get, ctx.dsAbstractions(fnc.id), uniformPrecision)
       specInputRanges += (fnc.id -> (specInputRanges(fnc.id) ++ transformed._2))
@@ -129,10 +189,14 @@ object ArithmeticUnrollPhase extends DaisyPhase {
       fnc.copy(body = Some(transformed._1), params = unrolledParamsFlattened)
     })
 
+    // what are the specInputRanges? we need to update them, too
+    val () = println("specInputRanges: " + specInputRanges)
+    
+
     println("Unrolled functions:")
     newDefs.foreach(println)
 
-    (ctx.copy(specInputRanges = specInputRanges,
+    (ctx.copy(specInputRanges = newspecInputRanges,
       specInputErrors = specInputErrors), Program(prg.id, newDefs ++ functionsToConsider(ctx, prg).diff(fncsToConsider)))
   }
 
@@ -210,10 +274,10 @@ object ArithmeticUnrollPhase extends DaisyPhase {
                     }
                 }
                 val zero = 0
-                var currLet = Let(FreshIdentifier(s"$i$zero", RealType), Times(Variable(FreshIdentifier(s"$t1Id$zero")), t2), b)
-                for(j <- 1 to vSize){
-                    val newId = FreshIdentifier(s"$i$j", RealType)
-                    val newLet = Let(newId, Times(Variable(FreshIdentifier(s"$t1Id$j")), t2), currLet)
+                var currLet = Let(getOrCreateFreshIdentifier(s"${i}_0", RealType), Times(Variable(getOrCreateFreshIdentifier(s"${t1Id}_0", RealType)), t2), b)
+                for(j <- 1 to vSize - 1){
+                    val newId = getOrCreateFreshIdentifier(s"${i}_$j", RealType)
+                    val newLet = Let(newId, Times(Variable(getOrCreateFreshIdentifier(s"${t1Id}_$j", RealType)), t2), currLet)
                     currLet = newLet
                 }
                 currLet
